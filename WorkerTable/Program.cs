0768e0006aa87e72877eff3996130a99c83d08c5
txger. <simaocampanico.dev@gmail.com>
2025-03-21 14:35:24 +0000

    Adding the project WorkerTable to the solution and working with the Bogus and Spectre.Console librarys

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+using Bogus.DataSets;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int count = int.Parse(args[0]);
+            Randomizer.Seed = new Random(count);
+            Faker faker = new Faker("pt_PT");
+
+            var table = new Table();
+
+            table.AddColumn("ID");
+            table.AddColumn("Name");
+            table.AddColumn("Job");
+
+            for (int i = 0; i < count; i++)
+            {
+                Name name = faker.Name;
+                table.AddRow($"{(i + 1)}", $"{name.FirstName()} {name.LastName()}", name.JobTitle());
+            }
+            AnsiConsole.Write(table);
+        }
+    }
+}
